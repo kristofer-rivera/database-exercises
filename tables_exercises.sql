929e85a837b3279f6e1f2db0b02b19958ad2de3a
Kristofer Rivera <kristofer.e.rivera@gmail.com>
2022-01-06 15:52:52 -0600

    initial commit of tables_exercises.sql

---
+++ tables_exercises.sql
@@ -0,0 +1,10 @@
+SHOW TABLES;
+DESCRIBE employees;
+-- Different data types available include: numeric, string, date, null, and a primary key. 
+-- I think dept_emp, employees, salaries, and titles contain numberic type columns
+DESCRIBE departments;
+DESCRIBE dept_emp;
+-- I think all the tables except salaries will contain string type columns
+-- I think all the tables except departments will contain date type columns
+-- In the dept_emp table we can see that the employee and department each have a number that correlates to eachother. 
+SHOW CREATE TABLE dept_manager;
